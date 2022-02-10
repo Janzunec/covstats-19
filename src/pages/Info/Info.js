a340f95b083366076761fc4ebc109c24515f5705
Jan Zunec <zunec.jan@gmail.com>
2022-02-10 12:26:33 +0100

    Created a file system, basic files to start.

---
+++ src/pages/Info/Info.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Info.css';
+
+export default function Info() {
+	return (
+		<div>
+			<h1>Info</h1>
+		</div>
+	);
+}
---
+++ src/pages/Stats/Statistcs.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import './Statistics.css';
+
+export default function Statistcs() {
+	return <div>Statistcs</div>;
+}
